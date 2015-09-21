a2cafbea05fc3f6709a5e47d71aeccb9025894cb
Chetan Mishra <cm5ky@virginia.edu>
2015-09-21 02:04:51 -0400

    y u no work nicely with gdal, heroku?

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'i+acxn5(akgsn!sr4^qgf(^m&*@+g1@u^t@=8s@axc41ml*f=s'
-GDAL_LIBRARY_PATH = '/app/.geodjango/gdal/lib/libgdal.so'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
